6f51f0cd078c6c021e1194ff76096a8e346588cb
ThomasAntens-bradford <t.antens@bradford-space.com>
2026-01-14 17:48:25 +0100

    small changes for versioning

---
+++ fmsdatabase/cli.py
@@ -0,0 +1,9 @@
+from fmsdatabase._version import version
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--version', action='store_true', help="Show version")
+    args = parser.parse_args()
+    if args.version:
+        print(f"fmsdatabase version {version}")
